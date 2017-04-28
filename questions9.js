70787605e4439e19aa835f38c87fde1711d3820c
fliptop2 <fliptop2@yahoo.com>
2017-04-28 10:09:41 +0800

    Add files via upload

---
+++ questions9.js
@@ -0,0 +1,3 @@
+﻿var myFlashcards = [     {question:"anticipation1 requires arranged or combined in the same way as in the claim",answer:
+"finisar and netmoneyin"},{question:"if several meanings disclosure points to proper meanings of claim
+terms",answer:"renishaw"} ];
