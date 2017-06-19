f31b26aec2df8bb97d118cd2aebdf80e41bcab7d
James Long <fliptop2@yahoo.com>
2017-06-19 14:38:07 +0800

    Update questions112.js

--- live-examples3/questions112.js
+++ live-examples3/questions112.js
@@ -1,4 +1,4 @@
-var myFlashcards = [     {question:"processor configured to perform various functions is a means-plus-function limitation",answer:"<a href="http://www.ptab.us/2014/09/lakkala-mass-inst-of-tech-aristocrat.html"><span style="color: #e69138;">lakkala</span></a>"}
+var myFlashcards = [     {question:"processor configured to perform various functions is a means-plus-function limitation",answer:"lakkala"}
      ];
 myFlashcards = document.getElementByClass("answer"), i, len = linkArray.length; for (i=0; i<len; i++){ myFlashcards.innerHTML += linkArray[i]; }  
 
